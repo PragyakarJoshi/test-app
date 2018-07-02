ca36f25a9669406e0edc559276a70e2cd0fe253b
Pragyakar Joshi <pragyakar75@gmail.com>
2018-07-02 12:33:16 +0545

    Email and Password Validation

--- js/script.js
+++ js/script.js
@@ -6,20 +6,47 @@ Vue.component('signUp', {
             newPassword:'',
             confirmPassword:'',
             disableSubmit: true,
+            error:[],
         }
     },
     watch: {
         email(value) {
             this.email = value;
             this.check_email(value);
+        },
+        newPassword(value) {
+            this.newPassword = value;
+            this.check_newPassword(value);
+        },
+        confirmPassword(value) {
+            this.confirmPassword = value;
+            this.check_confirmPassword(value);
         }
     },
     methods: {
         check_email(value){
             if (/^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/.test(value)){
-
+                this.error['email'] = '';
             } else {
-                
+                this.error['email'] = 'Enter Correct format of Email';
+            }
+        },
+        check_newPassword(value){
+            requiredChars = 6;
+            remainingChars = requiredChars - value.length;
+            if (value.length < requiredChars) {
+                this.error['newPassword'] = 'Atleast 6 characters required (' +remainingChars+ '/'+ requiredChars + ')' ;
+            } else {
+                this.error['newPassword'] = '';
+            }
+        },
+        check_confirmPassword(value){
+            if (value == this.newPassword){
+                this.error['confirmPassword'] = '';
+                this.disableSubmit = false;
+            } else {
+                this.error['confirmPassword'] = 'Passwords do not match';
+                this.disableSubmit = true;
             }
         }
     }
