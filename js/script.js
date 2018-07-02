3e7b6ac1c3acf2bdd0a3e129ff04292e9e7f4c66
Pragyakar Joshi <pragyakar75@gmail.com>
2018-07-02 17:35:16 +0545

    UI redesign

--- js/script.js
+++ js/script.js
@@ -1,3 +1,7 @@
+emailValidated = false;
+newPasswordValidated= false;
+confirmPasswordValidated = false;
+
 Vue.component('signUp', {
     template: '#signUp',
     data() {
@@ -27,25 +31,39 @@ Vue.component('signUp', {
         check_email(value){
             if (/^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/.test(value)){
                 this.error['email'] = '';
+                this.emailValidated = true;
             } else {
                 this.error['email'] = 'Enter Correct format of Email';
+                this.emailValidated = false;
             }
+            this.checkSubmit();
         },
         check_newPassword(value){
             requiredChars = 6;
             remainingChars = requiredChars - value.length;
             if (value.length < requiredChars) {
                 this.error['newPassword'] = 'Atleast 6 characters required (' +remainingChars+ '/'+ requiredChars + ')' ;
+                this.newPasswordValidated = false;
             } else {
                 this.error['newPassword'] = '';
+                this.newPasswordValidated = true;
             }
+            this.checkSubmit();
         },
         check_confirmPassword(value){
             if (value == this.newPassword){
                 this.error['confirmPassword'] = '';
-                this.disableSubmit = false;
+                this.confirmPasswordValidated = true;
             } else {
                 this.error['confirmPassword'] = 'Passwords do not match';
+                this.confirmPasswordValidated = false;
+            }
+            this.checkSubmit();
+        },
+        checkSubmit(){
+            if (this.emailValidated == true && this.newPasswordValidated == true && this.confirmPasswordValidated){
+                this.disableSubmit = false;
+            } else {
                 this.disableSubmit = true;
             }
         }
