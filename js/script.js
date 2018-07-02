b52d7f7011c56292f774245fecce77fd4b1c76e1
Pragyakar Joshi <pragyakar75@gmail.com>
2018-07-02 12:06:06 +0545

    template setup

--- js/script.js
+++ js/script.js
@@ -1,5 +1,28 @@
 Vue.component('signUp', {
-    template: '#signUp'
+    template: '#signUp',
+    data() {
+        return {
+            email:'',
+            newPassword:'',
+            confirmPassword:'',
+            disableSubmit: true,
+        }
+    },
+    watch: {
+        email(value) {
+            this.email = value;
+            this.check_email(value);
+        }
+    },
+    methods: {
+        check_email(value){
+            if (/^\w+([\.-]?\ w+)*@\w+([\.-]?\ w+)*(\.\w{2,3})+$/.test(value)){
+
+            } else {
+                
+            }
+        }
+    }
 });
 
 new Vue ({
